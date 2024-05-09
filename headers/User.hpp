3f1a55b18893f905c0a1c0053a65044aa2604e68
Nevesz7 <raphael.nevesz@live.com>
2024-05-09 13:36:56 -0300

    files

---
+++ headers/User.hpp
@@ -0,0 +1,45 @@
+/**======================
+*            42sp
+* @file      : User.hpp
+* @author    : luizedua
+* @email     : luizedua@student.42sp.org.br
+* @createdOn : 24/04/2024
+*========================**/
+#pragma once
+#ifndef User_HPP
+# define User_HPP
+# include "mainHeader.hpp"
+
+// class Channel;
+
+class User {
+	
+	private:
+		int			_userFD;
+		std::string	_realName;
+		std::string	_nickName;
+		std::string	_userIP;
+		std::string _password;
+		bool		_isAuth;
+
+	public:
+		//Getters and Setters
+		std::string	clientBuff;
+		void setuserFD(int userFD);
+		void setRealName(std::string realName);
+		void setNickName(std::string nickName);
+		void setuserIP(char * userIP);
+		
+		int			getuserFD();
+		std::string	getRealName();
+		std::string	getNickName();
+		std::string	getuserIP();
+		std::string	getPassword();
+		void		setPassword(std::string password);
+		void 		setIsAuth(bool isAuth);
+		bool		getIsAuth();
+
+		User(const char* nickname);
+};
+
+#endif
---
+++ headers/mainHeader.hpp
@@ -0,0 +1,38 @@
+/**======================
+*            42sp
+* @file      : mainHeader.hpp
+* @author    : luizedua
+* @email     : luizedua@student.42sp.org.br
+* @createdOn : 15/04/2024
+*========================**/
+#pragma once
+#ifndef INCLUDES_HPP
+# define INCLUDES_HPP
+# define MAX_USERS 666
+
+# include <iostream>
+# include <string>
+# include <list>
+# include <poll.h>
+# include <unistd.h>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <csignal>
+# include <fcntl.h>
+# include <limits>
+# include <cstdlib>
+# include <cstdio>
+# include <errno.h>
+# include <sstream>
+# include <cstring>
+# include <vector>
+# include <map>
+# include <algorithm>
+# include "error.hpp"
+# include "User.hpp"
+# include "Server.hpp"
+# include "Channel.hpp"
+
+#endif
