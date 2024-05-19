0ae3d23f1785b1d3581092a9c2e274e9945c43c1
Nevesz7 <raphael.nevesz@live.com>
2024-05-19 06:44:54 -0300

    kick

--- sources/operations/kick.cpp
+++ sources/operations/kick.cpp
@@ -11,7 +11,6 @@ void Server::kick(std::vector<std::string> options, int userFD) {
 
 	std::string kickReason;
     const std::string& channelName = options[0];
-	const std::string& userToBeKicked = options[1];
 	
 	if (options.size() == 3) { 
 		kickReason = options[2].substr(1);
@@ -21,7 +20,7 @@ void Server::kick(std::vector<std::string> options, int userFD) {
 
     Channel* channelPtr = getChannel(channelName);
     if (channelPtr == NULL) {
-		response = IRC + ERR_NOSUCHCHANNELNBR + channelName + ERR_NOSUCHCHANNEL + END;
+		response = IRC + ERR_NOSUCHCHANNELNBR + channelName + " " + channelName + ERR_NOSUCHCHANNEL + END;
 		send(userFD, response.c_str(), response.size(), 0);
 		return;
     }
@@ -32,10 +31,34 @@ void Server::kick(std::vector<std::string> options, int userFD) {
 		return;
     }
 
-	std::vector<User *> users = channelPtr->getAllUsers();
-	response = ":" + user->getNickName() + "!" + user->getRealName() + "@ft.irc KICK " + channelName + " " + userToBeKicked + " :"+ kickReason + END;
-	for (size_t i = 0; i < users.size(); i++) {
-		send(users[i]->getuserFD(), response.c_str(), response.size(), 0);
+	std::istringstream iss(options[1]);
+	std::string userToBeKickedNick;
+	while (std::getline(iss, userToBeKickedNick, ',')) {
+		User* userToBeKicked = getUserByNick(userToBeKickedNick);
+		if (userToBeKicked == NULL) {
+			response = IRC + ERR_NOSUCHNICKNBR + " " + channelName + " " + userToBeKickedNick + ERR_NOSUCHNICK + END;
+			send(userFD, response.c_str(), response.size(), 0);
+			continue;
+		}
+
+		if (!channelPtr->isUserOnChannel(userToBeKickedNick))
+		{
+			response = IRC + ERR_USERNOTINCHANNELNBR + channelName + " " + userToBeKickedNick + " " + channelName + ERR_USERNOTINCHANNEL + END;
+			send(userFD, response.c_str(), response.size(), 0);
+			continue;
+		}
+
+		if (!channelPtr->isUserOperator(user->getNickName())) {
+			response = IRC + ERR_CHANOPRIVSNEEDEDNBR + user->getNickName() + " " + channelName + ERR_CHANOPRIVSNEEDED + END;
+			send(userFD, response.c_str(), response.size(), 0);
+			return;
+		}
+
+		std::vector<User *> users = channelPtr->getAllUsers();
+		response = ":" + user->getNickName() + "!" + user->getRealName() + "@ft.irc KICK " + channelName + " " + userToBeKickedNick + " :"+ kickReason + END;
+		for (size_t i = 0; i < users.size(); i++) {
+			send(users[i]->getuserFD(), response.c_str(), response.size(), 0);
+		}
+		channelPtr->removeUser(userToBeKickedNick);
 	}
-    channelPtr->removeUser(userToBeKicked);
 }
